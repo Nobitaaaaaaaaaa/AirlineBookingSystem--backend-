91049abbd8466af556400a581cbe2b8ddacc426d
Ujjwal Kumar <123ujjwal.kr@gmail.com>
2026-07-12 21:29:08 +0530

    Made the routes, and databese

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const apiRoutes = require("./routes");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use('/api' , apiRoutes);;;;;;
+
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Server is running on port ${ServerConfig.PORT}`);
