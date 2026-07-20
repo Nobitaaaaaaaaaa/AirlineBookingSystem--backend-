91e03e7e952e7cebf858a612f61a2b7776963e48
Ujjwal Kumar <123ujjwal.kr@gmail.com>
2026-07-20 10:03:48 +0530

    Added the basic airport model without foreign key contrainst

---
+++ src/migrations/20260719191236-create-airport.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowedNull: false,
+        unique: true
+      },
+      code: {
+        type: Sequelize.STRING,
+        allowedNull: false,
+        unique: true
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowedNull: true
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowedNull: false
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
+};
