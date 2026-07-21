f41f7b7298c8af62d3f3519283bb2685dabfbb87
Ujjwal Kumar <123ujjwal.kr@gmail.com>
2026-07-21 18:15:29 +0530

    Added flights model  with foreign key constraints deined during model definition

--- src/index.js
+++ src/index.js
@@ -12,12 +12,7 @@ app.use('/api' , apiRoutes);;;;;;
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Server is running on port ${ServerConfig.PORT}`);
-    // bad code alert
-    const  { City , Airport } = require("./models");
-    // const city = await City.findByPk(1);
-    // console.log(city);
-    // const airport = await Airport.create({name:"John F. Kennedy International Airport", code: "JFK", address: "Queens, NY 11430, USA", city: "New York", cityId: 1});
-    // const city =  
+    
 
 });
 
